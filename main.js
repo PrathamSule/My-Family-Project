1fabed30fa597fe5c0e5803ad0d3fbeefd4e11eb
PrathamSule <73300111+PrathamSule@users.noreply.github.com>
2021-01-16 16:38:47 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,3 @@
+function Nextslide() {
+   document.getElementById(family).src="https://www.google.com/url?sa=i&url=https%3A%2F%2Fpolycount.com%2Fdiscussion%2F207700%2Fyoung-dad-cartoon-character-for-animation&psig=AOvVaw0Meh0kPyRQsBhs_EQpcvUw&ust=1610880685636000&source=images&cd=vfe&ved=0CAIQjRxqFwoTCJi37LakoO4CFQAAAAAdAAAAABAD";
+}
